95c0fbc21228cd037e1fbf308971030be99fd6bd
Sebastien Ros <sebastienros@gmail.com>
2024-10-21 09:09:50 -0700

    Add Playground project

---
+++ Silex.Playground/Program.cs
@@ -0,0 +1,23 @@
+﻿using Silex;
+using System.Diagnostics;
+
+if (Directory.Exists("db"))
+{
+    Directory.Delete("db", true);
+}
+
+var options = new StorageOptions { MemTableSizeLimit = 100.KiB() };
+var db = await LsmStorage.OpenAsync("db", options);
+
+var data = Enumerable.Range(0, 100_000).Select(x => new Bytes(x)).ToList(); 
+
+var sw = Stopwatch.StartNew();
+data.ForEach(x => db.Put(x, x));
+await db.CloseAsync();
+sw.Stop();
+
+Console.WriteLine(sw.Elapsed);
+foreach (var file in Directory.GetFiles("db"))
+{
+    Console.WriteLine($"{file} ({new FileInfo(file).Length})");
+}
